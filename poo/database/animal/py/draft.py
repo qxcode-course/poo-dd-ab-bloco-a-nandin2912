8ee2899ff94ba1a55f8f3fabfc00eb401040a139
nandin2912 <araujomenezesf@gmail.com>
2025-09-30 18:26:36 +0000

    atividade animal

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -6,16 +6,17 @@ class Animal:
         self.sound = sound 
 
     def toString(self):
-        return "f{self.species}:{self.age}:{self.sound}"
+        return f"{self.species}:{self.age}:{self.sound}"
     
     def ageBy(self, increment):
-        if self.age < 4 :
-            self.age =+ increment
+        if self.age + increment < 4:
+            self.age = increment + self.age
 
             if self.age > 4 :
                 self.age = 4
         else: 
             print(f"warning: {self.species} morreu")
+            self.age = 4
 
 
     def makeSound(self):
@@ -27,7 +28,38 @@ class Animal:
 
 
             
-        return "f{self.sound}"
+        return f"{self.sound}"
+
+
+
+
+def main():
+    animal = Animal("", "")
+    while True:
+        line: str = input()
+        print(f"${line}", end="\n")
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            species = args[1]
+            sound = args[2]
+            animal = Animal(species, sound)
+        elif args[0] == "grow":
+            animal.ageBy(int(args[1]))
+
+        elif args[0] == "noise":
+            print(animal.makeSound())
+
+
+        elif args[0] == "show":  
+            print(animal.toString())
+
+main()
+
+    
+
 
       
         
--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,4 +1,8 @@
-print("qual seu nome")
-nome: str = input()
-
-print(f"opa, seu nome é {nome}")
+class Carro:
+    def _init_(self):
+        self.pass_ = 0
+        self.passMax = 2
+        self.gas = 0
+        self.gasMax = 100
+        self.km = 0
+        
